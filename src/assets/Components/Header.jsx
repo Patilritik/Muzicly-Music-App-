c6bf17629584d05147153aa8f9b5fac982e4ea83
Patilritik <patilritik675@gmail.com>
2024-03-23 00:39:20 +0530

    header image

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
                     
                 <span class="flex-grow block border-t border-black "> </span>
                 <span class="flex  mx-4 px-4 py-2.5 text-4xl rounded-2xl leading-none font-medium bg-black text-white hover:shadow-lg hover:shadow-blue-300 transition duration-200 hover:scale-110 ">
-                 Muzicly  <img src="/src/assets/Images/listen.png" className=" ml-2 bg-transparent w-10 h-10 "  /> 
+                 Muzicly  <img src="/Images/listen.png" className=" ml-2 bg-transparent w-10 h-10 "  /> 
                 </span>
                 
                 <span class="flex-grow block border-t border-black"></span>
