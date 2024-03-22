b52176b34c1b23d537a830f201ce5997c148f30c
Patilritik <patilritik675@gmail.com>
2024-03-23 00:36:07 +0530

    t

--- src/assets/Home/Home.jsx
+++ src/assets/Home/Home.jsx
@@ -13,7 +13,7 @@ export default function Home(){
         </h1>
          <div className='flex flex-wrap justify-around my-20'>
             {/* <ArtistCard  name= "Arijit-Singh" image="/src/assets/Images/download.jpg"/> */}
-            <ArtistCard  name= "Arijit-Singh" image='/public/Images/download.jpg'/>
+            <ArtistCard  name= "Arijit-Singh" image='/Images/download.jpg'/>
             {/* <ArtistCard  name= "Atif Aslam" image="/images/Atif.jpg"/>
             <ArtistCard  name= "Jubin Nautiyal" image="/images/JUBIN.jpg"/>
             <ArtistCard  name= "Udit Narayan" image="/images/udit.jpg"/>
